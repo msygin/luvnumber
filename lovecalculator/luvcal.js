427ac1b83910ee103545ce9f5d0b1ef06b306919
Jason W Ellsworth <jasonellsworth48@gmail.com>
2019-06-06 17:27:14 +0800

    Add files via upload

---
+++ lovecalculator/luvcal.js
@@ -0,0 +1,6 @@
+let userName = prompt("What is your name?");
+let luvName = prompt("What is your partners name?");
+let luvCalc = Math.floor((Math.random() * 100)) + 1;
+
+
+alert(`${userName} your luv score with ${luvName} is ${luvCalc}%`);
